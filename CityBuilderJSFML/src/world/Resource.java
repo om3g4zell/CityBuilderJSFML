f227ae4acce36756d54ed8f8388ab6b05a293e3d
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2016-01-20 11:44:04 +0100

    Ajout de la JavaDoc pour la classe Resource et ResourcesMap

--- CityBuilderJSFML/src/world/Resource.java
+++ CityBuilderJSFML/src/world/Resource.java
@@ -1,10 +1,10 @@
 package world;
 
-/*
+/**
  * Represents a resource.
  */
 public class Resource {
-	// Resource type.
+	/** Resource type. */
 	public static enum ResourceType {
 		WATER,
 		ELECTRICITY,
@@ -16,19 +16,28 @@ public class Resource {
 		COUNT
 	}
 	
-	// Constructor.
+	/**
+	 * Constructor.
+	 * @param type : type of the resource
+	 */
 	public Resource(ResourceType type) {
 		this.type = type;
 		this.amount = 0.f;
 	}
 	
-	// Constructor with specific amount.
+	/**
+	 * Constructor with specific amount.
+	 * @param type : type of the resource
+	 * @param amount : amount of the resource
+	 */
 	public Resource(ResourceType type, float amount) {
 		this.type = type;
 		this.amount = amount;
 	}
 	
-	// Resource type and amount.
+	/** Resource type. */
 	public ResourceType type;
+	
+	/** Resource amount. */
 	public float amount;
 }
