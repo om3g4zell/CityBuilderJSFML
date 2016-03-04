9b7279e3fc6f24700beb380fed8a7f2f2e9025ed
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2016-03-04 18:43:04 +0100

    Ajout d'un champ 'smallFurnitureBuilingId' dans Citizen.

--- CityBuilderJSFML/src/world/Citizen.java
+++ CityBuilderJSFML/src/world/Citizen.java
@@ -1,33 +1,47 @@
 package world;
 
 public class Citizen {
-	
-	protected static int actualID  = 0;
+	protected static int lastID = 0;
 	
 	protected int id;
-	protected int houseId = -1;
-	protected int workBuildingId = -1;
+	protected int houseId = -1; // House.
+	protected int workBuildingId = -1; // Work ?
+	protected int smallFurnitureBuildingId = -1; // Grocery store.
 	
 	public Citizen(int houseId) {
 		this.houseId = houseId;
-		this.id = Citizen.actualID ++;
+		this.id = Citizen.lastID++;
 	}
 	
 	/**
-	 * Set the id of his workBuilding.
-	 * @param workBuildingId : the id of his workBuilding
+	 * Sets the id of the work building.
+	 * @param workBuildingId : the id of the workBuilding
 	 */
 	public void setWorkBuildingId(int workBuildingId) {
 		this.workBuildingId = id;
 	}
 	
 	/**
-	 * Get the id of his workBuilding
-	 * @return int : the id of his workBuilding
+	 * Returns the id of the work building.
+	 * @return the id of the workBuilding
 	 */
 	public int getWorkBuildingId() {
 		return this.workBuildingId;
 	}
 	
+	/**
+	 * Sets the id of the small furniture building.
+	 * @param smallFoodBuildingId : the id of the small furniture building
+	 */
+	public void setSmallFurnitureBuildingId(int smallFurnitureBuildingId) {
+		this.smallFurnitureBuildingId = smallFurnitureBuildingId;
+	}
 	
+	/**
+	 * Returns the id of the small furniture building.
+	 * @return the small furniture building id
+	 */
+	public int getSmallFurnitureBuildingId() {
+		return this.smallFurnitureBuildingId;
+	}
 }
