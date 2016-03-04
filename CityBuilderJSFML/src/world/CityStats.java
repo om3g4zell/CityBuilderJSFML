1351f50bdd996feb494030e35cc910dd43632e13
Louis Babuchon <louis.babuchon@etu.univ-tours.fr>
2016-03-04 16:18:57 +0100

    Calcul de l'attractivité de la ville dans cityStats

--- CityBuilderJSFML/src/world/CityStats.java
+++ CityBuilderJSFML/src/world/CityStats.java
@@ -1,15 +1,22 @@
 package world;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import world.Zone.ZoneClass;
 
 public class CityStats {
 	
 	protected int population;
 	protected int money;
+	protected Map<ZoneClass, Float> attractivity = new HashMap<ZoneClass, Float>();
 	
 	/**
-	 * 
-	 * @param buildings
+	 * Update the population number.
+	 * Update the money of the city.
+	 * Update the attractivity score of each zones.
+	 * @param buildings : City's buildings
 	 */
 	public void update(List<Building> buildings) {
 		this.population = 0;
@@ -19,12 +26,36 @@ public class CityStats {
 				this.population += 4;
 			}
 		}
+		for(Building building : buildings) {
+			for(ZoneClass zc : ZoneClass.values()) {
+				if(building.getZoneClasses().equals(zc)) {
+					attractivity.put(zc, attractivity.get(zc) + 1);
+				}
+			}
+		}
 	}
 	
+	/**
+	 * The attrativity score of a zone.
+	 * @param zoneClass
+	 * @return the score of the zone
+	 */
+	public float getAttractivity(ZoneClass zoneClass) {
+		return attractivity.get(zoneClass);
+	}
+	
+	/**
+	 * Get the population number.
+	 * @return int : population of the city
+	 */
 	public int getPopulation() {
 		return this.population;
 	}
 	
+	/**
+	 * Get the money of the city.
+	 * @return int : the money of the city
+	 */
 	public int getMoney() {
 		return this.money;
 	}
